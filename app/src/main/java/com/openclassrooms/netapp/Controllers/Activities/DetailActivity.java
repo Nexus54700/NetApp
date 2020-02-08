c7ac929f708a283a89fb8eb07f4170f01effb6ea
Nexus54700 <traceur54320@hotmail.fr>
2020-02-08 09:47:38 +0100

    creation of detailActivity and DetailFragment

---
+++ app/src/main/java/com/openclassrooms/netapp/Controllers/Activities/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.netapp.Controllers.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.openclassrooms.netapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/DetailFragment.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.netapp.Controllers.Fragments;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.openclassrooms.netapp.R;
+
+public class DetailFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_detail);
+    }
+}
